f163b78dbe641fcb5018af1d0fd46b8158f73de3
Sofia <73636331+SofiaPirogova@users.noreply.github.com>
2022-07-17 09:05:52 +0300

    1

---
+++ src/js/card-product/export-curiosity.js
@@ -0,0 +1,29 @@
+export default [ {
+	webp1x:
+	'../../images/webp/for-hydrates-1x.webp',
+webp2x:
+	'../../images/webp/for-hydrates-2x.webp',
+jpg1x: '../../images/jpg/for-hydrates-1x.jpg',
+jpg2x: '../../images/jpg/for-hydrates-2x.jpg',
+description: 'Медный чайник с фарфоровой ручкой',
+price:'1953 грн'
+},
+{
+webp1x:
+'../../images/webp/for-essential-oils-1х.webp',
+webp2x:
+'../../images/webp/for-essential-oils-2х.webp',
+jpg1x: '../../images/jpg/for-essential-oils-1х.jpg',
+jpg2x: '../../images/jpg/for-essential-oils-2х.jpg',
+description: 'Медный чайник с фарфоровой ручкой',
+price:'1953 грн'
+},
+{	webp1x:
+'../../images/webp/copper-utensils-1x.webp',
+webp2x:
+'../../images/webp/copper-accessories-2x.webp',
+jpg1x: '../../images/jpg/copper-utensils-1x.jpg',
+jpg2x: '../../images/jpg/copper-utensils-2x.jpg',
+description: 'Медный чайник с фарфоровой ручкой',
+price:'1953 грн'
+},]
